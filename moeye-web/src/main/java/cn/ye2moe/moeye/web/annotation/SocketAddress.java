f898ae59124a122e86e56bb0290cda468586f22c
ye2moe <1059654342@qq.com>
2018-08-24 14:54:23 +0800

    a word

---
+++ moeye-web/src/main/java/cn/ye2moe/moeye/web/annotation/SocketAddress.java
@@ -0,0 +1,10 @@
+package cn.ye2moe.moeye.web.annotation;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
+public @interface SocketAddress {
+}
